0692b26e331e569a33f192bbce8fecddb62cfb1c
William Baker <wbaker@gmail.com>
2020-06-19 12:35:37 -0400

    rb130 lesson material

---
+++ lesson_1/reduce.rb
@@ -0,0 +1,21 @@
+def reduce(array, init = nil)
+  output = init ? init : array[0]
+  idx = init ? 0 : 1
+
+  while idx < array.size
+    output = yield(output, array[idx])
+    idx += 1
+  end
+
+  output
+end
+
+array = [1, 2, 3, 4, 5]
+
+p reduce(array) { |acc, num| acc + num }                    # => 15
+p reduce(array, 10) { |acc, num| acc + num }                # => 25
+# p reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
+
+
+p reduce(['a', 'b', 'c']) { |acc, value| acc += value }     # => 'abc'
+p reduce([[1, 2], ['a', 'b']]) { |acc, value| acc + value } # => [1, 2, 'a', 'b']
---
+++ lesson_1/select.rb
@@ -0,0 +1,18 @@
+def select(arr)
+  output = []
+
+  idx = 0
+
+  while idx < arr.length
+    output << arr[idx] if yield(arr[idx])
+    idx += 1
+  end
+
+  output
+end
+
+array = [1, 2, 3, 4, 5]
+
+p select(array) { |num| num.odd? }      # => [1, 3, 5]
+p select(array) { |num| puts num }      # => [], because "puts num" returns nil and evaluates to false
+p select(array) { |num| num + 1 }       # => [1, 2, 3, 4, 5], because "num + 1" evaluates to true
